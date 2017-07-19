c919b4261550a60be208480d83ae2db4831105e7
YunS-Stacy <shiyun_stacy@yahoo.com>
2017-07-18 22:15:47 -0400

    add favicon

--- src/app/components/work-grid/work-grid.component.ts
+++ src/app/components/work-grid/work-grid.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class WorkGridComponent {
   tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
   ];
 }
